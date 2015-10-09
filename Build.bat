b48d03b4d8b55ffbd608449c45a285bb3a94006e
Amit Choudhary <amit_85i@hotmail.com>
2015-10-09 17:19:59 -0400

    Fixed path issue in build.bat

--- Build.bat
+++ Build.bat
@@ -15,15 +15,15 @@ if "%nuget%" == "" (
 )
 
 REM Package restore
-call %NuGet% restore WebHttpBehaviorExtensions.Tests\packages.config -OutputDirectory %cd%\packages -NonInteractive
+call %NuGet% restore src\WebHttpBehaviorExtensions.Tests\packages.config -OutputDirectory %cd%\packages -NonInteractive
 
 REM Build
-%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild WebHttpBehaviorExtensions.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild src\WebHttpBehaviorExtensions.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
 if not "%errorlevel%"=="0" goto failure
 
 REM Unit tests
 call %nuget% install NUnit.Runners -Version 2.6.4 -OutputDirectory packages
-packages\NUnit.Runners.2.6.4\tools\nunit-console.exe /config:%config% /framework:net-4.5 WebHttpBehaviorExtensions.Tests\bin\%config%\WebHttpBehaviorExtensions.Tests.dll
+packages\NUnit.Runners.2.6.4\tools\nunit-console.exe /config:%config% /framework:net-4.5 src\WebHttpBehaviorExtensions.Tests\bin\%config%\WebHttpBehaviorExtensions.Tests.dll
 if not "%errorlevel%"=="0" goto failure
 
 mkdir Build
@@ -32,7 +32,7 @@ mkdir Build\lib\net45
 
 REM Package
 mkdir Build
-call %nuget% pack "WebHttpBehaviorExtensions\WebHttpBehaviorExtensions.csproj" -symbols -o Build -p Configuration=%config% %version%
+call %nuget% pack "src\WebHttpBehaviorExtensions\WebHttpBehaviorExtensions.csproj" -symbols -o Build -p Configuration=%config% %version%
 if not "%errorlevel%"=="0" goto failure
 
 :success
