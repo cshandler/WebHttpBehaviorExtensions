482e5581e18e394548e184364bf555a7dce78452
vendettamit <amit_85i@hotmail.com>
2015-10-21 16:14:54 -0400

    Added custom authentication strategy

---
+++ src/WcfWebHttpIISHostingSample/Security/BasicAuthenticationProvider.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Web;
+using System.Web;
+using WebHttpBehaviorExtensions.Security;
+
+namespace WcfWebHttpIISHostingSample
+{
+    public class BasicAuthenticationProvider : IAuthenticationProvider
+    {
+        public bool Authenticate(System.ServiceModel.OperationContext operationContext)
+        {
+            //Extract the Authorization header, and parse out the credentials converting the Base64 string:
+            var authHeader = WebOperationContext.Current.IncomingRequest.Headers["Authorization"];
+
+            AuthenticationHeader header;
+
+            if (AuthenticationHeader.TryDecode(authHeader, out header))
+            {
+                /*
+                  This would be the place to inject the OAuth authentication manager. 
+                */
+
+                if ((header.Username == "user1" && header.Password == "test"))
+                {
+                    //User is authrized and originating call will proceed
+                    return true;
+                }
+                else
+                {
+                    //not authorized
+                    return false;
+                }
+            }
+            else
+            {
+                //No authorization header was provided, so challenge the client to provide before proceeding:
+                WebOperationContext.Current.OutgoingResponse.Headers.Add("WWW-Authenticate: Basic realm=\" WcfWebHttpIISHostingSample\"");
+                WebOperationContext.Current.OutgoingResponse.StatusCode = HttpStatusCode.Unauthorized;
+                return false;
+                //Throw an exception with the associated HTTP status code equivalent to HTTP status 401
+                //throw new WebFaultException(HttpStatusCode.Unauthorized);
+            }
+        }
+    }
+}
---
+++ src/WebHttpBehaviorExtensions/Security/Authorization/RestAuthorizationManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Web;
+using System.Threading;
+
+namespace WebHttpBehaviorExtensions.Security
+{
+    /// <summary>
+    /// Custom authorization manager for WCF Rest services which uses an ambient context to fetch the
+    /// authentication logic via IAuthenticationProvider
+    /// </summary>
+    public class RestAuthorizationManager : ServiceAuthorizationManager
+    {
+        protected override bool CheckAccessCore(OperationContext operationContext)
+        {
+            return AuthContext.Current.Authenticate(operationContext);
+        }
+    }
+}
